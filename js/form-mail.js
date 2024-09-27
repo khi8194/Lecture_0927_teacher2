1e9c97a9496941827b2cb806a99e7491055d1298
tubi55 <tubi55@nate.com>
2024-09-27 12:04:43 +0900

    폼메일 전송

--- js/form-mail.js
+++ js/form-mail.js
@@ -1,16 +1,25 @@
 emailjs.init({
-	publicKey: "YOUR_PUBLIC_KEY"
+	publicKey: "23g8RepczesqKPoIX"
 });
 
-document.getElementById("contact-form").addEventListener("submit", function (event) {
+//form선택자 변수 등록
+const form = document.querySelector("#contact-form");
+const user_name = form.querySelector("[name=user_name]");
+const user_email = form.querySelector("[name=user_email]");
+const message = form.querySelector("textarea");
+
+form.addEventListener("submit", event => {
 	event.preventDefault();
-	// these IDs from the previous steps
-	emailjs.sendForm("contact_service", "contact_form", this).then(
+	//sendForm(serviceID,  templateID, formEl)
+	emailjs.sendForm("service_iqrwpls", "template_nm3g0ec", form).then(
 		() => {
-			console.log("SUCCESS!");
+			user_name.value = "";
+			user_email.value = "";
+			message.value = "";
+			alert("문의 내용이 성공적으로 전달되었습니다.");
 		},
 		error => {
-			console.log("FAILED...", error);
+			alert("문의내용 전송에 실패했습니다.");
 		}
 	);
 });
